0ed1d1811e37ce3e13501deee9bdeb12e4902df5
Sofo Tabatadze <tabatadzesophie@gmail.com>
2024-02-23 15:56:45 +0400

    Update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const https = require("https");
-const fs = require("fs");
-const path = require("path");
+import https from "https";
+import fs from "fs";
+import path from "path";
 
 const PORT = process.env.PORT || 8000;
 
