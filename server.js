0c980dff233a2d2b5fc817b07a052f145f9acd7c
Sofo Tabatadze <tabatadzesophie@gmail.com>
2024-02-23 16:47:30 +0400

    Update

--- server.js
+++ server.js
@@ -1,35 +1,15 @@
-import https from "https";
-import fs from "fs";
-import path from "path";
+const jsonServer = require("json-server");
+const server = jsonServer.create();
 
-const PORT = process.env.PORT || 8000;
+// Path to JSON file
+const dataFile = "data/cities.json";
 
-const options = {
-  key: fs.readFileSync(path.join(__dirname, "ssl", "private.key")),
-  cert: fs.readFileSync(path.join(__dirname, "ssl", "certificate.crt")),
-};
-
-const server = https.createServer(options, (req, res) => {
-  if (req.url === "/cities" && req.method === "GET") {
-    // Construct the path to the cities.json file
-    const citiesFilePath = path.join(__dirname, "data", "cities.json");
-
-    // Read data from cities.json file
-    fs.readFile(citiesFilePath, (err, data) => {
-      if (err) {
-        res.writeHead(500, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ error: "Internal Server Error" }));
-      } else {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(data);
-      }
-    });
-  } else {
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ error: "Not Found" }));
-  }
+// Add custom routes if needed
+server.use("/custom-route", (req, res) => {
+  res.json({ custom: "response" });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// Use default router
+server.use(jsonServer.router(dataFile));
+
+module.exports = server;
